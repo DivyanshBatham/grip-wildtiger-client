4d8682b55c782f3934b5e630d487dee5d6e4dbcc
Divyansh Batham <divyansh.sunita@gmail.com>
2019-02-11 19:54:57 +0530

    Updates

---
+++ precache-manifest.b19144e989992e71912469ebaea79bf5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "820082a00dbc40640e964787d99b857e",
+    "url": "/grip-wildtiger-client/static/media/reservation-bg.820082a0.jpg"
+  },
+  {
+    "revision": "661c0fcd6f194ad66bade8df1fdd2dac",
+    "url": "/grip-wildtiger-client/static/media/paw.661c0fcd.svg"
+  },
+  {
+    "revision": "d7029b02c4bd24e635208122a1a54b86",
+    "url": "/grip-wildtiger-client/static/media/menu-bg.d7029b02.jpg"
+  },
+  {
+    "revision": "65249cef9671ef3528de8b4fa1566cf8",
+    "url": "/grip-wildtiger-client/static/media/home-bg.65249cef.png"
+  },
+  {
+    "revision": "75c039ee850217a01fe846a70a3f8f2b",
+    "url": "/grip-wildtiger-client/static/media/brush-stroke.75c039ee.svg"
+  },
+  {
+    "revision": "ab0f7cb523c13d72b970",
+    "url": "/grip-wildtiger-client/static/js/runtime~main.ab0f7cb5.js"
+  },
+  {
+    "revision": "819f93e18891b5f89c7b",
+    "url": "/grip-wildtiger-client/static/js/main.819f93e1.chunk.js"
+  },
+  {
+    "revision": "81b4ea8dc1adce6a831d",
+    "url": "/grip-wildtiger-client/static/js/1.81b4ea8d.chunk.js"
+  },
+  {
+    "revision": "819f93e18891b5f89c7b",
+    "url": "/grip-wildtiger-client/static/css/main.cf19c1f6.chunk.css"
+  },
+  {
+    "revision": "483ee50faac4c172103ddb3c28687934",
+    "url": "/grip-wildtiger-client/index.html"
+  }
+];
